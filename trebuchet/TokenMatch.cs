f029dce79a2efc24fd680d9b491f3a7697be7fb3
MartinMandzak <102599044+MartinMandzak@users.noreply.github.com>
2024-02-15 02:26:52 +0100

    i wanna kms rn

---
+++ trebuchet/TokenMatch.cs
@@ -0,0 +1,11 @@
+using clunt.trebuchet.Tokens;
+
+namespace cblunt.trebuchet.MyTokenizer{
+	public class TokenMatch{
+		public TokenType TokenType {get;set;}
+		public string Value {get;set;}
+		public int StartIndex {get;set;}
+		public int EndIndex {get;set;}
+		public int Precedence {get;set;}
+	}
+}

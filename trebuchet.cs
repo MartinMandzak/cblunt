ce85a8c8da107494186a5652aaa84e4cd70bde07
MartinMandzak <102599044+MartinMandzak@users.noreply.github.com>
2024-02-15 01:41:35 +0100

    first part of trebuchet working... YIPPEEEE

--- trebuchet.cs
+++ trebuchet.cs
@@ -8,13 +8,29 @@ using System.Collections;
 using System.IO;
 
 namespace DayOne{
+
+	public enum NumValues{
+		zero = 0,
+		one = 1,
+		two = 2,
+		three = 3,
+		four = 4,
+		five = 5,
+		six = 6,
+		seven = 7,
+		eight = 8,
+		nine = 9
+	}
+
 	class Trebuchet{
 		static void Main(string[] args){
 			ArrayList parsed_input = Take_input();
 			Console.WriteLine(parsed_input.Cast<int>().Sum());
 		}
 		private static ArrayList Take_input(){
-			char[] numbers = {'0','1','2','3','4','5','6','7','8','9'};
+			//NumValues numvals;
+			string[] numbers = { "0",		 "1",		 "2",		"3",	"4",	 "5",		 "6",		"7",  "8",  "9" ,
+													"nine","eight","seven","six","five","four","three","two","one","zero"};
 			ArrayList result = new ArrayList();
 			
 			try {
@@ -22,17 +38,17 @@ namespace DayOne{
 
 					string line;
 					while((line = sr.ReadLine()) != null){
-						Console.WriteLine(line);
-						char first='a'; char last='z';
-						for(int idx = 0; idx < line.Length / 2; idx ++){
-							if(first == 'a' && Array.IndexOf(numbers, line[idx]) != -1){
-								first = line[idx];
-							}
-							if(last == 'z' && Array.IndexOf(numbers, line[line.Length - idx - 1])!=-1){
-								last = line[line.Length - idx - 1];
+						string first="a"; string last="z";
+						foreach(char c in line){
+							string cc = c.ToString();
+							if(Array.Exists(numbers, x => x == cc)){
+								last = cc;
+								if(first == "a"){
+									first = cc;
+								}
 							}
 						}
-						result.Add(int.Parse(first.ToString() + last.ToString()));
+						result.Add(int.Parse(first+last));
 					}
 					return result;
 				}

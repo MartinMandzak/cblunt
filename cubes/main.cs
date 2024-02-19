46296a7150f491e94fcd306ae578bb2dcc019512
MartinMandzak <mandzak.martin3@gmail.com>
2024-02-19 14:09:41 +0100

    moving to big pc

---
+++ cubes/main.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Text;
+
+
+namespace Cubes{
+	class gameCalc{
+		static void Main(string[] args){
+			Console.WriteLine("i wanna kms");
+		}
+	}
+}
